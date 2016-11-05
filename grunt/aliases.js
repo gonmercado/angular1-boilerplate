4496bf606cfcf6ec8b5ccaa7bb91b51ef715eb10
Gonzalo Mercado <gonmercado@gmail.com>
2016-11-05 17:13:23 -0300

    Adding the configuration for grunt processhtml

--- grunt/aliases.js
+++ grunt/aliases.js
@@ -3,6 +3,7 @@
 
   module.exports = function(grunt){
     var cleanArray = [
+      //Removing old run of grunt
       'clean:gruntReports',
       'clean:app',
       'clean:frameworks'
@@ -11,18 +12,23 @@
     return {
       default: function(){
         var defaultTasks = cleanArray.concat([
+          //Running lints
           'eslint:xmlReport',
+          //Creating the concat and min version of the files and frameworks
           'concat:frameworks',
           'concat:cssFrameworks',
           'concat:app',
           'concat:css',
           'uglify:app',
-          'copy:generateIndexNonMin'
+          //Changing index file to use the min and concat version of files
+          'copy:generateIndexNonMin',
+          'processhtml:indexFile'
         ]);
 
         grunt.task.run(defaultTasks);
       },
       resetDev: cleanArray.concat([
+        //Restoring index file to the backup version
         'copy:generateIndex',
         'clean:indexNonMin'
       ])
