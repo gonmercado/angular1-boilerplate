09a38c3ae9ed0d595432f50905e2e87cc0f46e14
Gonzalo Mercado <gonmercado@gmail.com>
2016-11-10 02:18:38 -0300

    Testing test runs

--- test/ut/modules/authentication/components/login/login.controller.spec.js
+++ test/ut/modules/authentication/components/login/login.controller.spec.js
@@ -1,39 +1,41 @@
-'use strict';
+(function(){
+  'use strict';
 
-describe('Login Controller Suite', function(){
-  var ctrl, controller,
-    fakeState;
+  describe('Login Controller Suite', function(){
+    var ctrl, controller,
+      fakeState;
 
-  var generalInjections = function($controller){
-    controller = $controller;
-  };
+    var generalInjections = function($controller){
+      controller = $controller;
+    };
 
-  var prepareFakes = function(){
-    fakeState = {'go': function(){}};
-  };
+    var prepareFakes = function(){
+      fakeState = {'go': function(){}};
+    };
 
-  var createLoginController = function(){
-    ctrl = controller('LoginController', {
-      $state: fakeState
+    var createLoginController = function(){
+      ctrl = controller('LoginController', {
+        $state: fakeState
+      });
+    };
+
+    beforeEach(function(){
+      module('Authentication');
+      inject(generalInjections);
+      prepareFakes();
+      createLoginController();
     });
-  };
 
-  beforeEach(function(){
-    module('Authentication');
-    inject(generalInjections);
-    prepareFakes();
-    createLoginController();
+    describe('General Tests', function(){
+      it('Controller should be defined', function(){
+        expect(ctrl).toBeDefined();
+      });
+      it('Controller should have an authenticate method', function(){
+        expect(ctrl.authenticate).toBeDefined();
+      });
+      it('Text should be Login Page!', function(){
+        expect(ctrl.text).toEqual('Login Page!');
+      });
+    });
   });
-
-  describe('General Tests', function(){
-    it('Controller should be defined', function(){
-      expect(ctrl).toBeDefined();
-    });
-    it('Controller should have an authenticate method', function(){
-      expect(ctrl.authenticate).toBeDefined();
-    });
-    it('Text should be Login Page!', function(){
-      expect(ctrl.text).toEqual('Login Page!');
-    });
-  });
-});
+})();
