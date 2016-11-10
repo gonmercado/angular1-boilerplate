1bbb8b6396de9534604e1a088ce3f91c5af293c7
Gonzalo Mercado <gonmercado@gmail.com>
2016-11-10 02:06:09 -0300

    Adding git hook for running tests before commit

--- build/setup.sh
+++ build/setup.sh
@@ -3,3 +3,7 @@
 echo "Copying eslint commit hook"
 cp ./../scripts/githooks/commit-msg ./../.git/hooks/
 chmod u+x ./../.git/hooks/commit-msg
+
+echo "Copying test run before commit hook"
+cp ./../scripts/githooks/pre-commit ./../.git/hooks/
+chmod u+x ./../.git/hooks/pre-commit
